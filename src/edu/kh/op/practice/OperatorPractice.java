6d6dad9e18edb055c69df1127a7482e1cfd8b276
MINSU KIM <mskim9097@gmail.com>
2022-12-08 15:20:48 +0900

    fifth commit

--- src/edu/kh/op/practice/OperatorPractice.java
+++ src/edu/kh/op/practice/OperatorPractice.java
@@ -1,50 +1,73 @@
 package edu.kh.op.practice;
 
+import java.util.Scanner;
+
 public class OperatorPractice {
 	
 	public void practice1() {
-		int iNum1 = 29;
-		int iNum2 = 100;
 		
-		System.out.println("인원 수 : " + iNum1);
-		System.out.println("사탕 개수 : " + iNum2);
-		System.out.println();
-		System.out.println("1인당 사탕 개수 : " + iNum2 / iNum1);
-		System.out.println("남는 사탕 개수 : " + iNum2 % iNum1);
-		System.out.println("=============================");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("인원 수 : ");
+		int input = sc.nextInt();
+		
+		System.out.print("사탕 개수 : ");
+		int candy = sc.nextInt();
+		
+		System.out.println("1인당 사탕 개수 : " + candy / input);
+		System.out.println("남는 사탕 개수 : " + candy % input);
 	}
 	
 	public void practice2() {
-		String name1 = "홍길동";
-		int iNum3 = 3;
-		int iNum4 = 4;
-		int iNum5 = 15;
-		String sex = "남학생";
-		double grade = 85.75;
 		
-		System.out.println("이름 : " + name1);
-		System.out.println("학년 : " + iNum3);
-		System.out.println("반 : " + iNum4);
-		System.out.println("번호 : " + iNum5);
-		System.out.println("성별 : " + sex);
-		System.out.println("성적 : " + grade);
-		System.out.println();
-		System.out.println(iNum3 + "학년 " + iNum4 + "반 " + 
-						iNum5 + "번 " + name1 + " " +
-						sex + "의 성적은 " + grade + "이다.");
-		System.out.println("=============================");
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.print("이름 : ");
+		String name = sc.next();
+		
+		System.out.print("학년(정수만) : ");
+		int grade = sc.nextInt();
+		
+		System.out.print("반(정수만) : ");
+		int classroom = sc.nextInt();
+		
+		System.out.print("번호(정수만) : ");
+		int number = sc.nextInt();
+		
+		System.out.print("성별(남학생/여학생) : ");
+		String gender = sc.next();
+		
+		System.out.print("성적(소수점 아래 둘째 자리까지) : ");
+		double score = sc.nextDouble();
+
+		System.out.printf("%d학년 %d반 %d번 %s %s의 성적은 %.2f이다.", 
+				grade, classroom, number, name, gender, score);
 	}
 	
 	public void practice3() {
-		int iNum6 = 60;
-		int iNum7 = 80;
-		int iNum8 = 40;
+		
+		Scanner sc = new Scanner(System.in);
 
-		System.out.println("국어 : " + iNum6);
-		System.out.println("영어 : " + iNum7);
-		System.out.println("수학 : " + iNum8);
+		System.out.print("국어 : ");
+		int kor = sc.nextInt();
+		
+		System.out.print("영어 : ");
+		int eng = sc.nextInt();
+		
+		System.out.print("수학 : ");
+		int math = sc.nextInt();
+		
+		int sum = kor + eng + math;
+		double avg = sum / 3.0;
+		
+		boolean result = (kor >= 40) && (eng >= 40) 
+				&& (math >= 40) && (avg >= 60);
+
+		System.out.println("합계 : " + sum);
+		System.out.printf("평균 : %.1f", avg);
 		System.out.println();
-		System.out.println("합계 : " + (iNum6 + iNum7 + iNum8));
-		System.out.println("평균 : " + (double)((iNum6 + iNum7 + iNum8) /3));
+		System.out.println(result ? "합격" : "불합격");
+		// 조건식 ? 식1 : 식2
 	}
+
 }
